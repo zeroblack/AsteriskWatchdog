c93de733564368b8a893fc69c0919f433e648bdb
root <root@7labs.local>
2013-09-03 16:43:58 -0400

    Added command line arguments for Twitter's credentials

--- dog.py
+++ dog.py
@@ -5,24 +5,35 @@ import subprocess
 import tweepy
 import datetime
 import time
+import argparse
 
-popen = subprocess.Popen(['/usr/sbin/asterisk', '-rx', 'sip show peers'], stdout=subprocess.PIPE)
+# Check config options
+parser = argparse.ArgumentParser(description='Tweet an alert when a PBX extension goes down')
+parser.add_argument('--notweet', help='Active when a tweet alert should be triggered', action='store_true')
+parser.add_argument('--ck', help='Consumer key')
+parser.add_argument('--cs', help='Consumer secret')
+parser.add_argument('--at', help='Access token')
+parser.add_argument('--ats', help='Access token secret')
 
+args = parser.parse_args()
+
+print args
+# Check PBX extensions status
+popen = subprocess.Popen(['/usr/sbin/asterisk', '-rx', 'sip show peers'], stdout=subprocess.PIPE) 
 out, err = popen.communicate()
 
-# fill these variables with your twitter auth data
-consumer_key="your consumer key"
-consumer_secret="your consumer secret"
-access_token="your access token"
-access_token_secret="your access token secret"
-
+# Twitter config data
+consumer_key=args.ck
+consumer_secret=args.cs
+access_token=args.at
+access_token_secret=args.ats
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
-
 api = tweepy.API(auth)
 
+# Trigger needed alerts
 for client in out.split('\n'):
    if not client.startswith('Name') and client.find('sip') == -1 and client.find('/') != -1 and client.find('OK') == -1:
       print client[:8] + client[-15:]
-      #I advice you to mention your twitter handle here
-      api.update_status('@yourHandle ' + client[:8] + client[-15:] + datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S'))
+      if not args.notweet:
+         api.update_status('@sdrgalvis ' + client[:8] + client[-15:] + datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S'))
